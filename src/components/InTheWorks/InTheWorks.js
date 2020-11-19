dd6e61af7e7ec4c161e3674c7c14ab4f9cd0e070
Stav Kidron <kidronstav@gmail.com>
2020-11-19 11:41:51 -0600

    GET route for songs working and displaying on the dom

--- src/components/InTheWorks/InTheWorks.js
+++ src/components/InTheWorks/InTheWorks.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import './InTheWorks.css';
+import {Jumbotron, Container, Col, Row, ListGroup, ListGroupItem, Button} from 'reactstrap';
 
 // Basic class component structure for React with default state
 // value setup. When making a new component be sure to replace
@@ -8,10 +10,58 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 // component.
 class InTheWorks extends Component {
 
+  componentDidMount = () => {
+    this.getSongs();
+  }
+
+  getSongs = () => {
+    this.props.dispatch({type: 'GET_SONGS'});
+  }
+
   render() {
     return (
       <div>
-        <h2>In-The-Works</h2>
+        <Jumbotron>
+          <Container>
+            <Col xs="8">
+              <h1>In-The-Works</h1>
+            </Col>
+          </Container> 
+        </Jumbotron>
+
+        <Container>
+          <ListGroup>
+            {/* {JSON.stringify(this.props.store.songs)} */}
+            {this.props.store.songs.map((song) => {
+              return(
+                <ListGroupItem key={song.id} className="itwListItem">
+                  <div>
+                    <Row>
+
+                      <Col xs="8">
+                        <h3>{song.title}</h3>
+                        <p>{song.date}</p>
+                      </Col>
+
+                      <Col xs="4" className="buttonCol">
+                        <Button color="primary" size="sm" className="itwBtns">
+                          Edit Song
+                        </Button>
+                        <div></div>
+                        <Button color="primary" size="sm" className="itwBtns">
+                          View Song
+                        </Button>
+                      </Col>
+
+                    </Row>
+                  </div>        
+                </ListGroupItem>
+              )
+            })}
+          </ListGroup>
+        </Container>
+        
+
       </div>
     );
   }
