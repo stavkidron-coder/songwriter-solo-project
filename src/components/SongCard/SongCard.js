2938e0a4578d910f37b6372cf7680abba99f7c99
Stav Kidron <kidronstav@gmail.com>
2020-11-27 16:53:49 -0600

    styled cards

--- src/components/SongCard/SongCard.js
+++ src/components/SongCard/SongCard.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import './SongCard.css';
 import DropDownBtns from '../DropDownBtns/DropDownBtns';
 import {Row, Col} from 'reactstrap';
 
 class SongCard extends Component {
     render(){
         return(
-            <div key={this.props.song.id} className="homeListItem">
+            <div key={this.props.song.id} className="songCardContainer">
                 <Row>
 
                   <Col xs="8">
@@ -13,10 +14,10 @@ class SongCard extends Component {
                     <p>{this.props.song.date}</p>
                   </Col>
 
-                  <Col xs="4" className="homeButtonCol">
+                  <Col xs="4" className="songCardButtonCol">
                     <DropDownBtns song={this.props.song}/> 
                   </Col>
-                  
+
                 </Row>
                      
             </div>
